0aee34ee7dd5c2bccfd0e4c170367e875a42206a
yuanyuan.wang <yuanyuan.wang@chinaredstar.com>
2017-11-17 16:55:54 +0800

    使用mockjs工具模拟数据

--- api/db.js
+++ api/db.js
@@ -1,7 +1,17 @@
+const Mock=require('mockjs')
+const Random=Mock.Random
+
 module.exports=function(){
   const data={users:[]}
+  const names=[1,2,3,4].map((item)=>Random.cname(item,item+1))
   for(let i=0,len=5;i<len;i++){
-    data.users.push({id:i,name:'wyy'+i})
+    let name=names[Random.integer(1,3)]
+    data.users.push({
+      id:i,
+      name:name,
+      email:Random.email(),
+      website:Random.url()
+    })
   }
   return data
 }
